4950c1a746c7585264200ef77a04ab0563ba7f6d
Manuel Garcia <m.gd@outlook.es>
2024-04-21 22:56:55 +0200

    creacion de registro y acctualizacion de comunicacion entre ventanas

--- src/app/pantallas/recuperar/recuperar.component.ts
+++ src/app/pantallas/recuperar/recuperar.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recuperar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './recuperar.component.html',
   styleUrl: './recuperar.component.css'
 })
--- src/app/pantallas/registro/registro.component.ts
+++ src/app/pantallas/registro/registro.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
